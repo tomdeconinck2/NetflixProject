a0da5e1558229389b10425c21615379d4e67530a
tomdeconinck2 <tom.de.coninck@devoteam.com>
2020-10-01 17:09:56 +0200

    communication between backend and frontend

--- Frontend/Netflix/src/app/services/login.service.ts
+++ Frontend/Netflix/src/app/services/login.service.ts
@@ -3,34 +3,43 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 //import {Observable} from 'rxjs/Observable';
 
 const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class LoginService {
-
 	x;
 
-  constructor(private http:HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  getLogin() {
-    let token = localStorage.getItem('access_token');
-    return this.http.get('/server/login'//,
-     // {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)}
-    );
-  }
+	getLogin() {
+		let token = localStorage.getItem('access_token');
+		return this.http.get('/server/login'//,
+			// {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)}
+		);
+	}
 
 	ping() {
 		console.log("Service pinged ")
-		
-		this.x = this.http.get('http://localhost:8080/test');
+
+		this.x = this.http.get('/server/test');
 		console.log(this.x)
 		return this.x
 		//return this.http.get('/server/test1')
+
+	}
+
+	getBestMovie() {
+		console.log("Method not implemented.");
+		//this.x = this.http.get('http://localhost:8901');
+		this.x = this.http.get('/movieserver/');
 		
+		console.log(this.x);
+		console.log(" - - -");
+		return this.x;
 	}
 
 
