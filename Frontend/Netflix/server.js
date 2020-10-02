7247639e65885e60a037f02efc5ca1afc1889124
tomdeconinck2 <tom.de.coninck@devoteam.com>
2020-10-02 11:27:02 +0200

    heroku

---
+++ Frontend/Netflix/server.js
@@ -0,0 +1,23 @@
+//angular.module('app',['ngRoute','customersApp.loginController']);
+
+//app.use(express.static('./dist/netflix'));
+//app.get('/*', function(req, res) { res.sendFile('index.html', {root: 'dist/netflix/'} );});
+
+//app.listen(process.env.PORT || 8080);
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
