ee84abb6815cbce7e4ba4139ebe78f4da5f8f0d0
tomdeconinck2 <tom.de.coninck@devoteam.com>
2020-10-02 11:59:59 +0200

    fix2

--- Frontend/Netflix/server.js
+++ Frontend/Netflix/server.js
@@ -12,12 +12,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/netflix'));
+app.use(express.static(__dirname + '/dist/Netflix'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/netflix/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Netflix/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
