e6bfa86c429c0e857a7a9fc59a80eae11d0fa090
Roy De Prins <roydeprins@Roys-MacBook-Pro.local>
2020-10-02 15:07:21 +0200

    added Sleuth to test ms

--- test/src/main/java/com/NetflixProject/test/controller/TestController.java
+++ test/src/main/java/com/NetflixProject/test/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.NetflixProject.test.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,12 @@ public class TestController {
 	
 	@Value("${msg:Config Server is not working. Please check...}")
     private String msg;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
  
     @GetMapping()
     public String getMsg() {
+    	logger.info("sleuth test");
         return this.msg;
     }
 	
