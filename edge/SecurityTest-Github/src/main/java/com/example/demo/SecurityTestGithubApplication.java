9a43665936e07ea3493602036e7ba5ccde83fa57
tomdeconinck2 <tom.de.coninck@devoteam.com>
2020-10-05 17:12:37 +0200

    Started with security

---
+++ edge/SecurityTest-Github/src/main/java/com/example/demo/SecurityTestGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTestGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTestGithubApplication.class, args);
+	}
+
+}
