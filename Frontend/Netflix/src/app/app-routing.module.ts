30e1dbc3ace7c1cf7614ff48c319d6357d04db2f
Roy De Prins <roydeprins@Roys-MacBook-Pro.local>
2020-10-09 13:51:46 +0200

    started frontend for user-ms

--- Frontend/Netflix/src/app/app-routing.module.ts
+++ Frontend/Netflix/src/app/app-routing.module.ts
@@ -3,19 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { MainComponent } from './components/main/main.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
 	{
 		path: 'movies',
 		component: MovieComponent
 	},
-	 {
+	{
 		path: '',
 		component: MainComponent
 	},
-		{
+	{
 		path: 'login',
 		component: LoginComponent
+	},
+	{
+		path: 'user',
+		component: UserComponent
 	},
 
 
---
+++ Frontend/Netflix/src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(){
+    console.log('Method getUsers in user.service.ts')
+    return this.http.get('server/user-service/all')
+  }
+
+  getWelcomeMessage(){
+    return this.http.get('server/user-service/')
+  }
+
+  //TODO delete/save/... methods
+
+}
