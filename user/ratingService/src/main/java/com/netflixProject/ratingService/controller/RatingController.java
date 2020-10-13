c899427edf3eb1b35fb56d23f35be257915fd7c4
Roy De Prins <roy.de.prins@devoteam.com>
2020-10-13 15:50:45 +0200

    a rating can now be changed + added comments to clearify the code

--- user/ratingService/src/main/java/com/netflixProject/ratingService/controller/RatingController.java
+++ user/ratingService/src/main/java/com/netflixProject/ratingService/controller/RatingController.java
@@ -21,33 +21,51 @@ public class RatingController {
 	@Autowired
 	private RatingService ratingService;
 	
+	/*
+	 * return a welcome message from this microservice
+	 */
 	@GetMapping("/")
 	public String welcome() {
 		return "Welcome to the rating service!";
 	}
 	
+	/*
+	 * Return a list of all ratings in the database
+	 */
 	@GetMapping("/all")
 	public List<Rating> getAllRatings() {
 		return ratingService.getAllRatings();	
 	}
 	
+	/*
+	 * Get all ratings from a specific user
+	 */
 	@GetMapping("/ratingsOfUser/{userId}")
 	public List<Rating> getRatingsForUser(@PathVariable("userId") Long userId){
 		List<Rating> ratings = this.getAllRatings();
 		return ratings.stream().filter(rating -> userId.equals(rating.getUserId())).collect(Collectors.toList());
 	}
 	
+	/*
+	 * Get the rating of given id
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<Rating> getRating(@PathVariable("id") Long id) {
 		return ratingService.getRating(id);
-	}
+	}	
 	
-	
+	/*
+	 * Add the given rating to the database.
+	 * If the rating already exists the old value will be overwritten
+	 */
 	@PostMapping("/addRating")
 	public ResponseEntity<String> addRating(@RequestBody Rating rating) {
 		return ratingService.addRating(rating);
 	}
 	
+	/*
+	 * Delete the rating with given id
+	 */
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteRating(@PathVariable("id") Long id){
 		return ratingService.deleteRating(id);
