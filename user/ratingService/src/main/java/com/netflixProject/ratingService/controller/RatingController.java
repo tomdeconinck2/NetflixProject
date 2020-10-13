52528dbdf25179230ab0777b14220447446ac5a4
Roy De Prins <roy.de.prins@devoteam.com>
2020-10-13 13:41:24 +0200

    fixed small bug in RatingController

--- user/ratingService/src/main/java/com/netflixProject/ratingService/controller/RatingController.java
+++ user/ratingService/src/main/java/com/netflixProject/ratingService/controller/RatingController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,8 +43,7 @@ public class RatingController {
 	}
 	
 	
-	
-	@GetMapping("/addRating")
+	@PostMapping("/addRating")
 	public ResponseEntity<String> addRating(@RequestBody Rating rating) {
 		return ratingService.addRating(rating);
 	}
