7278d4b2ab27ce8ebe34467383deb8edb2b52df5
Roy De Prins <roy.de.prins@devoteam.com>
2020-10-15 16:50:45 +0200

    Made the user page refresh when adding or deleting a rating

--- Frontend/Netflix/src/app/components/specific-user/specific-user.component.ts
+++ Frontend/Netflix/src/app/components/specific-user/specific-user.component.ts
@@ -16,7 +16,6 @@ export class SpecificUserComponent implements OnInit {
   user;
   ratings;
   error = "/";
-  refresh = "nope";
 
   validMessage: string = "/";
 
@@ -27,13 +26,15 @@ export class SpecificUserComponent implements OnInit {
     this.user_id = Number(this.route.snapshot.paramMap.get("id"));
     this.getUserInfo();
     this.getRatingsFromUser();
+    this.initialiseRatingForm();
+  }
 
+  initialiseRatingForm(){
     this.ratingForm = new FormGroup({
       userId: new FormControl(this.user_id),
       movieId: new FormControl('', Validators.required),
       rating: new FormControl('', Validators.required)
     })
-
   }
 
   getUserInfo(): void{
@@ -47,28 +48,23 @@ export class SpecificUserComponent implements OnInit {
   getRatingsFromUser(): void{
     this.userService.getRatingsFromUser(this.user_id).subscribe(
 			data => { console.log("Data"); console.log(data);  this.ratings = data },
-			err => { console.log("Err"); console.error(err); this.ratings = err.error.text },
-		);
+			err => { console.log("Err"); console.error(err); this.error = err.error.text },
+    );
   }
 
   addRating(){
-    if(this.ratingForm.valid){
-      this.validMessage = "Your new rating has been succesfully added. Thank you!";
-      this.ratingService.addRating(this.ratingForm.value).subscribe(
-        data => { this.ratingForm.reset(); return true},
-        error => { this.error=error.err.text;}
-      )
-    } 
-    else{
-      this.validMessage = "Please fill out the form before submitting!";
-    }
-
+    this.validMessage = "Your new rating has been succesfully added. Thank you!";
+    this.ratingService.addRating(this.ratingForm.value).subscribe();
+    this.ratingForm.reset();
+    this.initialiseRatingForm();
+    this.getRatingsFromUser();
   }
 
   //TODO update the page so the deleted rating is not visible anymore.
   // window.location.reload() does not work because it is run before the deleteRating function terminates...
   deleteRating(rating_id){
     this.ratingService.deleteRating(rating_id).subscribe();
+    this.getRatingsFromUser();
   }
 
 }
